d8c2a81e8d03ec9b5dc2a84676c0cd71186837a8
Priyanka Sadagopan <priyankasadagopan@Priyankas-MacBook-Pro.local>
2025-02-07 10:16:41 -0600

    Just need to add title and desc to home page

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+
 
 // This is a functional component that represents a single menu item. It currently takes in the title and displays it in an h2 element.
 // Modify the component to take in all the other properties of a menu item you need and display them in the component.
 // Use bootstrap to style the elements so that it looks like the mockup in the assignment.
 // Hint: You can use the image name to get the image from the images folder.
-const MenuItem = ({title, description, imagename}) => {
+const MenuItem = ({title, description, imagename, price}) => {
     return (
         <div>
             <div className = 'menuContainer'>
@@ -21,7 +23,15 @@ const MenuItem = ({title, description, imagename}) => {
                     </div>
                 </Col>
                 <Col xs={7} sm={7} md ={7}>
-                    <h1>{title}</h1>
+                <div className = 'titledesc'>
+                    <h5>{title}</h5>
+                    <h6>{description}</h6>
+                </div>
+                    
+                    <div className = 'price'>
+                        <p>${price}  <Button variant="outline-info" size ="small">Add</Button> </p>
+                       
+                    </div>
                 </Col>
                 </Row>
             </Container>
